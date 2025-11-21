b7f02d1cee8d14ba944207a0c65bd2ea441b2bba
ratul0407 <rajaulislamratul12@gmail.com>
2025-11-21 21:40:50 +0600

    created prescription route

--- src/app/modules/prescription/prescription.controller.ts
+++ src/app/modules/prescription/prescription.controller.ts
@@ -2,21 +2,21 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { PrescriptionService } from "./prescription.service";
+import { IJwtPayload } from "../../types/common";
 
-const createPrescription = catchAsync(async (req: Request, res: Response) => {
-  // const user = req.user;
-  const result = await PrescriptionService
-    .createPrescription
-    //   user as IJwtPayload,
-    //   req.body
-    ();
-  sendResponse(res, {
-    success: true,
-    statusCode: 201,
-    message: "Prescription created successfully!",
-    data: result,
-  });
-});
+const createPrescription = catchAsync(
+  async (req: Request & { user?: IJwtPayload }, res: Response) => {
+    const user = req.user;
+    const result = await PrescriptionService.createPrescription;
+    user as IJwtPayload, req.body();
+    sendResponse(res, {
+      success: true,
+      statusCode: 201,
+      message: "Prescription created successfully!",
+      data: result,
+    });
+  }
+);
 
 export const PrescriptionController = {
   createPrescription,
--- src/app/modules/prescription/prescription.service.ts
+++ src/app/modules/prescription/prescription.service.ts
@@ -1,5 +1,48 @@
-const createPrescription = async () => {
-  return {};
+import {
+  AppointmentStatus,
+  PaymentStatus,
+  Prescription,
+  UserRole,
+} from "@prisma/client";
+import { IJwtPayload } from "../../types/common";
+import { prisma } from "../../shared/prisma";
+import ApiError from "../../errors/apiError";
+import httpStatus from "http-status";
+const createPrescription = async (
+  user: IJwtPayload,
+  payload: Partial<Prescription>
+) => {
+  const appointmentData = await prisma.appointment.findUniqueOrThrow({
+    where: {
+      id: payload.appointmentId,
+      status: AppointmentStatus.COMPLETED,
+      paymentStatus: PaymentStatus.PAID,
+    },
+    include: {
+      doctor: true,
+      patient: true,
+    },
+  });
+  if (user.role === UserRole.DOCTOR) {
+    if (!(user.email === appointmentData.doctor.email)) {
+      throw new ApiError(
+        httpStatus.BAD_REQUEST,
+        "This is not your appointment"
+      );
+    }
+  }
+  return await prisma.prescription.create({
+    data: {
+      appointmentId: appointmentData.id,
+      doctorId: appointmentData.doctor.id,
+      patientId: appointmentData.patient.id,
+      instructions: payload.instructions as string,
+      followUpDate: payload.followUpDate || null,
+    },
+    include: {
+      patient: true,
+    },
+  });
 };
 export const PrescriptionService = {
   createPrescription,
